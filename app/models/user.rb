7588da9e0fbce778cb9922f23bd1e0954c6b7e85
tetsuya_kanamaru <t.kanamaru1221@gmail.com>
2020-08-04 22:16:01 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password
+    validates :password_confirmation
+    validates :family_name
+    validates :given_name
+    validates :family_name_reading
+    validates :given_name_reading
+    validates :birthday
+  end
+
+  validates :email, inclusion: {in: ['@']},
+                    uniqueness: true
+
+  with_options length: {minimum: 6, message: 'パスワードは６文字以上を入力してください'},format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'パスワードは半角英数字を含めて入力してください' do
+    validates :password
+    validates :password_confirmation
+  end
+
+  with_options format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角ひらがな、またはカタカナ、または漢字で入力してください'} do
+    validates :family_name
+    validates :given_name
+  end
+
+  with_options format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください'}
+  validates :family_name_reading
+  validates :given_name_reading
+
+end
+
+validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_08_04_120004) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", null: false
+    t.string "password", null: false
+    t.string "password_confirmation", null: false
+    t.string "family_name", null: false
+    t.string "given_name", null: false
+    t.string "family_name_reading", null: false
+    t.string "given_name_reading", null: false
+    t.date "birthday", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
