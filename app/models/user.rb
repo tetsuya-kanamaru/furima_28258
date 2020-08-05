453a278e7c3825019e356599fb056df77a27d94c
tetsuya_kanamaru <t.kanamaru1221@gmail.com>
2020-08-05 11:31:53 +0900

    userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,34 +6,13 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :email
-    validates :password
-    validates :password_confirmation
-    validates :family_name
-    validates :given_name
-    validates :family_name_reading
-    validates :given_name_reading
+    validates :email, inclusion: {in: ['@']}, uniqueness: true
+    validates :password, length: {minimum: 6, message: 'パスワードは６文字以上を入力してください'}, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'パスワードは半角英数字を含めて入力してください'}
+    validates :password_confirmation, length: {minimum: 6, message: 'パスワードは６文字以上を入力してください'}, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'パスワードは半角英数字を含めて入力してください'}
+    validates :family_name, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角ひらがな、カタカナ、または漢字で入力してください'}
+    validates :given_name, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角ひらがな、カタカナ、または漢字で入力してください'}
+    validates :family_name_reading, format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください'}
+    validates :given_name_reading, format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください'}
     validates :birthday
   end
-
-  validates :email, inclusion: {in: ['@']},
-                    uniqueness: true
-
-  with_options length: {minimum: 6, message: 'パスワードは６文字以上を入力してください'},format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'パスワードは半角英数字を含めて入力してください' do
-    validates :password
-    validates :password_confirmation
-  end
-
-  with_options format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角ひらがな、またはカタカナ、または漢字で入力してください'} do
-    validates :family_name
-    validates :given_name
-  end
-
-  with_options format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください'}
-  validates :family_name_reading
-  validates :given_name_reading
-
 end
-
-  validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
-
