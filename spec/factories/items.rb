f354629ba336509ef9db656884c99c8eb64b4336
tetsuya_kanamaru <t.kanamaru1221@gmail.com>
2020-08-11 12:39:02 +0900

    指摘箇所修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :item do
 
-    name              {Faker::Lorem.words}
-    item_description  {Faker::Lorem.sentence}
-    category_id       {Faker::Number.within(range: 1..10)}
-    item_condition_id {Faker::Number.within(range: 1..6)}
-    shipping_fee_id   {Faker::Number.within(range: 1..2)}
-    shipping_place_id {Faker::Number.within(range: 1..47)}
-    shipping_days_id  {Faker::Number.within(range: 1..3)}
-    price             {Faker::Number.within(range: 300..9999999)}
+    name              {'デスク'}
+    item_description  {'デスクです'}
+    category_id       {3}
+    item_condition_id {3}
+    shipping_fee_id   {2}
+    shipping_place_id {4}
+    shipping_days_id  {2}
+    price             {100000}
 
     association :user
 
