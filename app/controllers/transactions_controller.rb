6dc50dda7909c27bbce7c1a39568e6cafc84b1b2
tetsuya_kanamaru <t.kanamaru1221@gmail.com>
2020-08-12 20:42:03 +0900

    未ログインだとログインページに、ログインかつ出品者が出品商品の購入画面に遷移しようとしたらトップページにリダイレクトすること

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,7 +6,10 @@ class TransactionsController < ApplicationController
   end
 
   def move_to_login
-    unless user_signed_in?
+    @item = Item.find(params[:item_id])
+    if user_signed_in? && @item.user_id == current_user.id
+        redirect_to root_path
+    else
       redirect_to new_user_session_path
     end
   end
